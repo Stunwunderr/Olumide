38afb2f55fa15f4c4f4f3498065199f83f9f391a
Stunwunderr <90192072+Stunwunderr@users.noreply.github.com>
2023-08-13 08:18:03 +0100

    Update BannerImg.js

--- src/containers/greeting/BannerImg.js
+++ src/containers/greeting/BannerImg.js
@@ -1,38 +1,26 @@
-//import lottie from 'lottie-web';
-//import React, {useRef,useEffect } from "react";
+import lottie from 'lottie-web';import React, {useRef,useEffect } from "react";
 import React, { Component } from "react";
-import banner from "./olumide.jpg"
-import "./Greeting.css";
+// import banner from "./olumide.jpg"
+// import "./Greeting.css";
 
 //for animated svgs use json file
-// export default function BannerImg(){
- //   const container = useRef(null)
-//    useEffect(() => {
- //     lottie.loadAnimation({
- //       container: container.current,
- //       renderer: 'svg',
-//        loop: true,
- //       autoplay: true,
-  //      animationData: require('./banner.json')
-  //    })
-  //  }, [])
- //   return (
-  //    <div className="App">
- //       <div className="container" ref={container}></div>
- //     </div>
-//    );
-//}
+export default function BannerImg(){
+    const container = useRef(null)
+    useEffect(() => {
+      lottie.loadAnimation({
+        container: container.current,
+        renderer: 'svg',
+      loop: true,
+       autoplay: true,
+       animationData: require('./banner.json')
+     })
+  }, [])
+   return (
+      <div className="App">
+        <div className="container" ref={container}></div>
+     </div>
+    );
+}
 
 
- class BannerImg extends Component {
-   render() {
-     return (
-       <div className="App">
-         <div className="container">
-       <img src={banner} alt="" />
-       </div>
-       </div>
-     );
-   }
- }
- export default BannerImg;
+ 
